def5b939428816a0d7a6730f120542e8a1bd6056
oleg-kucherenko <developer.olegkucherenko@outlook.com>
2021-03-19 15:31:04 +0200

    Updated

---
+++ build/precache-manifest.a1a8950dd071f9869189334ae19e1561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ecb23090e1b009b55475f92e6256bdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8bb5a4c09ecc728f004",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "f8bb5a4c09ecc728f004",
+    "url": "/static/js/2.a49cdb36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a49cdb36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccf8fb9f398d7cdba77b",
+    "url": "/static/js/main.65a75c4e.chunk.js"
+  },
+  {
+    "revision": "e480acbfb3a4cf2af9ab",
+    "url": "/static/js/runtime-main.782177d8.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+app.use(express.static(path.join(__dirname, 'build')))
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server works on port ${port}`))
